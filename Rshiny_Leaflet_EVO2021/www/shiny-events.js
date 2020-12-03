026cfe8771e70700dec0856c9d49069687cd9b0a
and.reea <33720256+Deea53@users.noreply.github.com>
2020-12-03 19:24:46 +0000

    Rshiny_basic

---
+++ Rshiny_Leaflet_EVO2021/www/shiny-events.js
@@ -0,0 +1,33 @@
+$(function() {
+    var renders = 0;
+    var press_play = false;
+    $(document).on({
+
+        // COULD ALSO DO $('#slider_div').on('shiny:value', function() {...})
+        'shiny:value': function(event) {
+            switch (event.name) {
+                case 'slider_div':
+                    // WONT RESUME IF THE RENDER WAS DUE TO SOMETHING ELSE
+                    if (renders > 0 && press_play) {
+                        setTimeout(function() {
+                            console.log('Animation Speed Changed!');
+                            $('.slider-animate-button').click();
+                        }, 200);
+
+                        press_play = false;
+                    } else {
+                      renders = 1;
+                    }
+                    break;
+
+                default:
+            } 
+        }
+
+    });
+
+
+    Shiny.addCustomMessageHandler('resume', function(message) {
+        press_play = true;
+    });
+});
